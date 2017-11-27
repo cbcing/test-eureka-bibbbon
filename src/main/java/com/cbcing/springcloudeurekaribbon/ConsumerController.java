62f242259bf01f7d1fcd76cdf1af216709fe7588
David <davvvvvvidchen@gmail.com>
2017-11-27 14:30:37 +0800

    增加服务容错的保护:hystrix／修改相应的错误名称

--- src/main/java/com/cbcing/springcloudeurekaribbon/ConsumerController.java
+++ src/main/java/com/cbcing/springcloudeurekaribbon/ConsumerController.java
@@ -4,17 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private HelloService helloService;
 
     @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
     public String helloConsumer() {
-        return restTemplate.getForEntity("http://TEST-SPRING-CLOUD-EUREKA-SERVICE-PROVIDER/hello", String.class).getBody();
+        return helloService.helloService();
     }
 
 }
